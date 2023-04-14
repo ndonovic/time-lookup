e842ccfae83dbc40c075cb0497751c8321e28e1d
Nik Donovic <32178288+ndonovic@users.noreply.github.com>
2023-04-13 22:00:38 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+(function() {
+    var cors_api_host = 'cors-anywhere.herokuapp.com';
+    var cors_api_url = 'https://' + cors_api_host + '/';
+    var slice = [].slice;
+    var origin = window.location.protocol + '//' + window.location.host;
+    var open = XMLHttpRequest.prototype.open;
+    XMLHttpRequest.prototype.open = function() {
+        var args = slice.call(arguments);
+        var targetOrigin = /^https?:\/\/([^\/]+)/i.exec(args[1]);
+        if (targetOrigin && targetOrigin[0].toLowerCase() !== origin &&
+            targetOrigin[1] !== cors_api_host) {
+            args[1] = cors_api_url + args[1];
+        }
+        return open.apply(this, args);
+    };
+})();
+
 async function fetchTimeZone() {
   const locationInput = document.getElementById('location').value;
   const apiKey = '641db438b18b474b90545f84a84f1085';
